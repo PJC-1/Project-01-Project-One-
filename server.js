62fdb90e0200ed34d6320fe3b773886e22cafb8c
PJC-1 <jumpzine@gmail.com>
2016-04-07 11:42:28 -0700

    Linked files, jQY, HndBrs, BStr

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-//server.js
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Routes
+
+//HTML Endpoints
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/view/index.html');
+});
+
+//JSON API Endpoints
+
+//server
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
