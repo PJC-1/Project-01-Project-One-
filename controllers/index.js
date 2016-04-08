ae89d6cb30e8b257a06c2e2348656235700947d7
PJC-1 <jumpzine@gmail.com>
2016-04-07 23:05:53 -0700

    display seed data success

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,3 @@
 module.exports.api = require('./apiController');
+// module.exports.Category = require('./categoryController');
+module.exports.items = require('./itemController');
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,1 +1,16 @@
+var db = require('../models');
 
+
+
+
+//GET /api/items
+function index(req, res) {
+  db.Item.find({}, function(err, data){
+    res.json(data);
+  });
+}
+
+//export public methods here
+module.exports = {
+  index: index
+};
