8d64dda2a96fcadb15972532320120fb3fd7c99a
PJC-1 <jumpzine@gmail.com>
2016-04-12 06:58:25 -0700

    spring cleaning app js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,71 +15,35 @@ $(document).ready(function() {
     $(this).trigger("reset");
   });
 
-
-  //EVENT CLICK UPDATE-BUTTON
   $('#items').on('click', '.update-item', function(e) {
-    console.log('update-item clicked!');
     var currentItemId= $(this).closest('.item').data('item-id');
-    console.log('logging id: ',currentItemId);
     $('#itemModal').data('item-id', currentItemId);
     $('#itemModal').modal();
   });
-  //END EVENT CLICK UPDATE-BUTTON
 
-  //EVENT SAVE-MODAL
   $('#saveModal').on('click', saveModalSuccess);
+
   $('#items').on('click', '.delete-item', deleteItemClick);
 
-  // $('#saveModal').on('click', function(e) {
-  //   e.preventDefault();
-  //   var formData = $('formModal').serialize();
-  //   console.log('formData', formData);
-  //   var $modal = $('#itemModal');
-  //   var itemId = $modal.data('itemId');
-  //   var itemUrl = '/api/items/' + itemId;
-  //   $.ajax({
-  //     method: 'PUT',
-  //     url: itemUrl,
-  //     data: formData,
-  //     success: updateSuccess,
-  //     error: updateError
-  //   });
-  //
-  //   $modal.modal('hide');
-  //   $(this).trigger("reset");
-  // });
-
 });
-//End of Doc-Ready
-
-
-
-
-
-
 
 function saveModalSuccess(e) {
   e.preventDefault();
-
-  console.log("You have clicked the save changes button!");
   var $modal = $('#itemModal');
   var $description = $modal.find('#description');
   var $condition = $modal.find('#condition');
   var $importance_level = $modal.find('#importance_level');
   var $category = $modal.find('#category');
-
   var dataCategory = {category: $category.val()};
-  // var dataCategory = $category.val();
   var dataToPost = {
     description: $description.val(),
     condition: $condition.val(),
     importance_level: $importance_level.val(),
   };
-
   dataToPost.category = dataCategory;
   var itemId = $modal.data('itemId');
-  console.log("This is category: ", dataToPost);
   var itemUrl = '/api/items/' + itemId;
+
   $.ajax({
     method: 'PUT',
     url: itemUrl,
@@ -93,25 +57,15 @@ function saveModalSuccess(e) {
   $importance_level.val('');
   $category.val('');
   $modal.modal('hide');
-
 }
 
-
-
-
-//UPDATE SUCCESS/ERROR
 function updateSuccess(data){
-  console.log(data);
-  console.log('received data from update:', data);
   var itemId = data._id;
-  console.log("this is the data ", data);
   $('div[data-item-id=' + itemId + ']').remove();
   renderItem(data);
 }
-
 function updateError(){
   console.log("error");
-
 }
 
 function deleteItemClick(e) {
