5d34f48fa2e882859f3638abb3655d95f3ae1263
PJC-1 <jumpzine@gmail.com>
2016-04-12 07:02:14 -0700

    spring cleaning server js

--- server.js
+++ server.js
@@ -7,22 +7,16 @@ var controllers = require("./controllers");
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-//Routes
-
-//HTML Endpoints
 app.get('/', function homepage (req, res) {
   res.sendFile(__dirname + '/view/index.html');
 });
 
-//JSON API Endpoints
 app.get('/api', controllers.api.index);
 app.get('/api/items', controllers.items.index);
 app.get('/api/items/:itemId', controllers.items.show);
 app.post('/api/items', controllers.items.create);
 app.put('/api/items/:itemId', controllers.items.update);
 app.delete('/api/items/:itemId', controllers.items.destroy);
-//server
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is running on http://localhost:3000/');
